028d5a2cbcc783b9c23aaa44640214ebe95d82ad
eddo81 <eduardo_jonnerstig@live.com>
2019-10-20 10:24:27 +0200

    summery2

--- create-nova-theme.js
+++ create-nova-theme.js
@@ -114,7 +114,7 @@ const run = async () => {
           message: "Enter the theme's version number:",
           initial: `1.0.0`,
           validate: value =>
-            /^\d{1,2}\.\d{1,2}\.\d{1,2}$/.test(value) === false
+            /^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}$/.test(value) === false
               ? `Invalid version format, must be sequence of either single or doubble digits followed by a period.`
               : value
         },
