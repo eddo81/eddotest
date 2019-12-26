ec527ec158b232efd5daefa07512dec38175d138
eddo81 <eduardo_jonnerstig@live.com>
2019-12-27 00:49:54 +0100

    updates

--- src/templates/theme/copy/inc/back-compat.php
+++ src/templates/theme/copy/inc/back-compat.php
@@ -21,7 +21,7 @@ if ( defined( 'ABSPATH' ) === false ) {
  *
  * @return string Message to show to the user.
  */
-function <%= prefix %>_get_requirements_message() {
+function get_requirements_message() {
 	$incorrect_wp_version  = version_compare( MIN_WP_VERSION, get_bloginfo('version'), '>=' );
 	$incorrect_php_version = version_compare( MIN_PHP_VERSION, phpversion(), '>=' );
 
@@ -48,23 +48,22 @@ function <%= prefix %>_get_requirements_message() {
  *
  * @param array $options The error message body.
  */
-function <%= prefix %>_render_compatibility_error( $options = array() ) {
-	$message = esc_html( <%= prefix %>_get_requirements_message() );
+function render_compatibility_error( $options = array() ) {
+	$message = esc_html( get_requirements_message() );
 	$heading = __( 'Compatibility error', '<%= textDomain %>' );
 	$title   = $heading;
-	<%= prefix %>_theme_error($message, $heading, $title, $options);
+	theme_error($message, $heading, $title, $options);
 }
 
 /**
  * Prevents switching to the theme when requirements are not met, falls back to the default theme.
  */
-function <%= prefix %>_switch_theme() {
+function switch_theme() {
 	switch_theme( WP_DEFAULT_THEME );
 	unset( $_GET['activated'] );
-	add_action( 'admin_notices', '<%= prefix %>_upgrade_notice' );
+	add_action( 'admin_notices', 'upgrade_notice' );
 }
-add_action( 'after_switch_theme', '<%= prefix %>_switch_theme' );
-
+add_action( 'after_switch_theme', 'switch_theme' );
 
 /**
  * Adds a message for unsuccessful theme switch.
@@ -72,44 +71,44 @@ add_action( 'after_switch_theme', '<%= prefix %>_switch_theme' );
  * Prints an update nag after an unsuccessful attempt to switch to the theme
  * when requirements are not met.
  */
-function <%= prefix %>_upgrade_notice() {
-	printf( '<div class="error"><p>%s</p></div>', esc_html( <%= prefix %>_get_requirements_message() ) );
+function upgrade_notice() {
+	printf( '<div class="error"><p>%s</p></div>', esc_html( get_requirements_message() ) );
 }
 
 /**
  * Prevents the Customizer / Front-end from being loaded when requirements are not met.
  */
-function <%= prefix %>_customize() {
+function customize() {
 	$options = array( 'back_link' => true );
-	<%= prefix %>_render_compatibility_error( $options );
+	render_compatibility_error( $options );
 }
-add_action( 'load-customize.php', '<%= prefix %>_customize' );
+add_action( 'load-customize.php', 'customize' );
 
 /**
  * Prevents the Theme Preview from being loaded when requirements are not met.
  */
-function <%= prefix %>_preview() {
+function preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		<%= prefix %>_render_compatibility_error();
+		render_compatibility_error();
 	}
 }
-add_action( 'template_redirect', '<%= prefix %>_preview' );
+add_action( 'template_redirect', 'preview' );
 
 
 /**
  * Deactivate current theme when requirements are not met, falls back to the default theme.
  */
-function <%= prefix %>_init_theme() {
+function init_theme() {
 
 	if ( is_admin() ) {
 		$options = array( 'back_link' => true );
-		<%= prefix %>_switch_theme();
-		<%= prefix %>_render_compatibility_error( $options );
+		switch_theme();
+		render_compatibility_error( $options );
 	} else {
 		$title   = __( 'Compatibility error', '<%= textDomain %>' );
 		$message = __( 'There is a compatibility issue which is preventing this page from rendering properly.', '<%= textDomain %>' );
-		<%= prefix %>_theme_error( $message, $title );
+		theme_error( $message, $title );
 	}
 
 }
-add_action( 'init', '<%= prefix %>_init_theme' );
+add_action( 'init', 'init_theme' );
